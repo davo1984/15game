6716ce496c28ae52dd788928a197187edda92530
David Oliver <davooliver1964@gmail.com>
2020-04-11 21:26:54 -0400

    shuffle works, winner detected, not displaying

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button, Row} from 'reactstrap';
+import {Button, Row, Alert} from 'reactstrap';
 import Board from './Components/Board.js';
 import SwapTiles from './Components/SwapTiles.js';
 
@@ -18,9 +18,7 @@ class App extends Component {
   }
   
   resetTiles() {
-    // console.log('inside resetTiles');
     let tempTiles = this.state.tiles;
-    // console.log('inside resetBoard', tempTiles);
     for (let i = 0; i < tempTiles.length; i++) {
       let findTileMethod = (element) => element.name === i + 1;
       let tileLocation = this.state.tiles.findIndex(findTileMethod);
@@ -30,46 +28,52 @@ class App extends Component {
   }
 
   shuffleTiles() {
-    // console.log('inside shuffleTiles');
     let tempTiles = this.state.tiles;
     let shuffleCtr = 0;
     let newEmptyLocation = 14;
     let emptyLocation = tempTiles.findIndex((e) => e.isEmpty === true);
     do {
       let previousLocation = emptyLocation;
-      let randomMove =  Math.floor(Math.random() * 3);
+      let randomMove =  Math.round(Math.random() * 3);
       if (randomMove !== previousLocation) {
-        if (randomMove === 0 && emptyLocation > 0) {
+        if (randomMove === 1 && emptyLocation > 0) {
           newEmptyLocation = emptyLocation - 1;
           tempTiles = this.SwapTiles(newEmptyLocation, emptyLocation, tempTiles);
           shuffleCtr++;
-        } else if (randomMove === 1 && emptyLocation < 15) {
+        } else if (randomMove === 3 && emptyLocation < 14) {
           newEmptyLocation = emptyLocation + 1;
           tempTiles = this.SwapTiles(newEmptyLocation, emptyLocation, tempTiles);
           shuffleCtr++;
-        } else if (randomMove === 2 && emptyLocation < 11) {
-          newEmptyLocation = emptyLocation - 4;
-          tempTiles = this.SwapTiles(newEmptyLocation, emptyLocation, tempTiles);
-          shuffleCtr++;
-        } else if (randomMove === 3 && emptyLocation < 4) {
+        } else if (randomMove === 2 && emptyLocation <= 11) {
           newEmptyLocation = emptyLocation + 4;
           tempTiles = this.SwapTiles(newEmptyLocation, emptyLocation, tempTiles);
           shuffleCtr++;
+        } else if (randomMove === 0 && emptyLocation >= 4) {
+          newEmptyLocation = emptyLocation - 4;
+          tempTiles = this.SwapTiles(newEmptyLocation, emptyLocation, tempTiles);
+          shuffleCtr++;
         }
-        // console.log = ('shuffle', axisX, axisY);
         previousLocation = newEmptyLocation;
         emptyLocation = newEmptyLocation;
       }
-    } while (shuffleCtr < 50);
+    } while (shuffleCtr < 250);
     this.setState({ tiles: tempTiles });
   }
 
   handleTilesChange(tiles) {
     this.setState({ tiles });
+    for ( let i=0; i<this.state.tiles.length; i++ ) {
+      if ( this.state.tiles[i].name != i+1 ) { return }
+    }
+    console.log('WINNER');
+    return (
+    <Alert color="success">
+      SUCCESS! You Are a WINNER!
+    </Alert>
+    )
   }
   
   componentDidMount() {
-    // console.log('inside componentDidMount');
     // TODO add async & wait    how?    is needed?
     for ( let i=0; i < ( this.state.numRows * this.state.numCols ); i++ ) {
       let isSpace = false;
@@ -109,7 +113,7 @@ class App extends Component {
             <div className="text-center">
               <Button color="success" size="lg" onClick={this.shuffleTiles}>Shuffle</Button>{' '}
               {/* &nbsp;&nbsp;&nbsp;  This will add space between the buttons */}
-              <Button color="info" size="lg">Slow Shuffle</Button>{' '}
+              {/* <Button color="info" size="lg">Slow Shuffle</Button>{' '} */}
               <Button color="warning" size="lg" onClick={this.resetTiles}>Reset</Button>
               </div>
           </footer>
--- src/Components/SwapTiles.js
+++ src/Components/SwapTiles.js
@@ -1,10 +1,10 @@
 function SwapTiles(startLocation, endLocation, tempTiles) {
-  let tempName = tempTiles[endLocation].name;
-  let tempIsEmpty = tempTiles[endLocation].isEmpty;
-  tempTiles[endLocation].name = tempTiles[startLocation].name;
-  tempTiles[endLocation].isEmpty = tempTiles[startLocation].isEmpty;
-  tempTiles[startLocation].name = tempName;
-  tempTiles[startLocation].isEmpty = tempIsEmpty;
+    let tempName = tempTiles[endLocation].name;
+    let tempIsEmpty = tempTiles[endLocation].isEmpty;
+    tempTiles[endLocation].name = tempTiles[startLocation].name;
+    tempTiles[endLocation].isEmpty = tempTiles[startLocation].isEmpty;
+    tempTiles[startLocation].name = tempName;
+    tempTiles[startLocation].isEmpty = tempIsEmpty;
   return tempTiles;
 }
 export default SwapTiles;
